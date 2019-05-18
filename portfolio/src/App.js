940d4ee08bc0a9b071cda340c61d804c399a206b
ashik71 <a.ashrafe71@gmail.com>
2019-05-18 12:51:11 +0600

    Update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
-import HeaderBody from './components/headerbody';
+import Footer from './components/Footer';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Testimonials from './components/Testimonials';
+import Portfolio from './components/Portfolio';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+  }
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
   render() {
     return (
-      <div>
-      <Header/>
-      <HeaderBody/>
-      </div>
+      <div className="App">
+      <Header data={this.state.resumeData.main}/>
+      <About  data={this.state.resumeData.main}/>
+      <Resume />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
     );
   }
 }
--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return (           
-            <div className="container-fluid info p-3" id="top-icons">
-            <div className="row">
-              <div className="col d-flex justify-content-between align-items-baseline flex-wrap">
-                <div className="info-icons p-2">
-                  <a href="#" className="mr-2 primary-color">
-                  <i className="fab fa-facebook-f fa-2x"></i>              
-                  </a>
-                  <a href="#" className="mr-2 primary-color">              
-                  <i className="fab fa-github fa-2x"></i>
-                  </a>
-                </div>
-              </div>
-            </div>    
-    
-          </div>      
-          
-        );
+
+  render() {
+    if (this.props.data) {
+      var name = this.props.data.name;
+      var occupation = this.props.data.occupation;
+      var description = this.props.data.description;
+      var city = this.props.data.address.city;
+      var networks = this.props.data.social.map(function (network) {
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
     }
+
+    return (
+      <React.Fragment>
+        <header id="home">
+
+          <nav id="nav-wrap">
+
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+            <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+            <ul id="nav" className="nav">
+              <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+              <li><a className="smoothscroll" href="#about">About</a></li>
+              <li><a className="smoothscroll" href="#resume">Resume</a></li>
+              <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+              <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+              <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            </ul>
+
+          </nav>
+
+          <div className="row banner">
+            <div className="banner-text">
+              <h1 className="responsive-headline">I'm {name}.</h1>
+              <h3>I'm a <span>{occupation}</span>.{description}.</h3>
+              <hr />
+              <ul className="social">
+                {networks}
+              </ul>
+              <p className="scrolldown">
+                <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+              </p>
+            </div>
+
+
+          </div>
+
+          {/* <p className="scrolldown">
+            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+          </p> */}
+
+        </header>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Header;
